9e07e77b7a40a855d082a717fcdd49053a202876
Nawaporn Kietweerachon <nkietwee@z3t1c3.42bangkok.com>
2023-12-09 15:02:01 +0700

    update cpp00_ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -4,27 +4,42 @@
 
 // using namespace std;
 
-int main(int ac, char**av)
+int main(int ac, char **av)
 {
-    char c;
-
-    if (ac == 1)
-        std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
-    else
-    {
-        for (int i = 1; i < ac; i++)
-        {
-            for (int j = 0; j < strlen(av[i]); j++)
-            {
-                if (isalpha(av[i][j]))
-                {
-                    c = toupper(av[i][j]);
-                    std::cout << c ;
-                }
-                else
-                    std::cout << av[i][j];
-            }
-        }
-    }
-
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl ;
+	else
+	{
+		for (int i = 1; i < ac ; i++)
+		{
+			for (int j = 0; j < strlen(av[i]); j++)
+			{
+				std::cout << (char)toupper(av[i][j]);
+			}
+		}
+		std::cout << std::endl;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
