4cb3abcd28ac9bae3c8175c5a168a97ea5245de3
nkietwee <nawaporn.kiet@gmail.com>
2024-08-28 17:21:38 +0700

    update cpp07/ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nkietwee <nkietwee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 12:19:18 by nkietwee          #+#    #+#             */
-/*   Updated: 2024/08/27 13:00:27 by nkietwee         ###   ########.fr       */
+/*   Updated: 2024/08/28 17:20:21 by nkietwee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,24 @@ int main(void)
 {
     int a = 2;
     int b = 3;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
     
+    // ::swap( a, b );
+    swap( a, b );
     std::cout << "Original" << std::endl;
     std::cout << "a = " << a << ", b = " << b << std::endl; 
-    
-    ::swap( a, b );
+   
     std::cout << "Swap" << std::endl;
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
     
-    std::string c = "chaine1";
-    std::string d = "chaine2";
+    swap(c, d);
     std::cout << std::endl;
     std::cout << "Original" << std::endl;
-    std::cout << "c = " << c << ", d = " << d << std::endl; 
-    
-    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+     
     std::cout << "Swap" << std::endl;
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
